43768953cee0468f84193feca7cfc95062d3e8e8
Charles <chalice.chu@gmail.com>
2024-09-27 00:46:34 -0400

    feat: style setting

--- frontend/layouts/v0/index.vue
+++ frontend/layouts/v0/index.vue
@@ -1,6 +1,14 @@
 <template>
-    <div class="w-screen h-screen">
+    <div :class="cn('w-screen h-screen flex flex-col', props.innerClass ?? '')">
         <Navbar />
         <slot />
     </div>
 </template>
+
+<script setup lang="ts">
+import { cn } from '~/lib/utils';
+
+const props = defineProps<{
+    innerClass?: string;
+}>();
+</script>
--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,7 +1,24 @@
 <template>
     <NuxtLayout name="v0">
-        <div class=" ">
+        <div class="mx-auto grow flex gap-14 bg-gray-50 rounded shadow-sm px-10 py-8">
+            <div class="flex flex-col grow gap-8">
+                <div>
+                    <p class="text-3xl font-bold">
+                        Youlin Qu
+                    </p>
+                    <p class="text-xs text-gray-800">
+                        Pittsburgh, PA | 207-286-7026 | chalice.chu@gmail.com
+                    </p>
+                </div>
+                <div>experience</div>
+                <div>Education</div>
+                <div>projects</div>
+            </div>
 
+            <div class="flex flex-col gap-8">
+                <div>skills</div>
+                <div>languages</div>
+            </div>
         </div>
     </NuxtLayout>
 </template>
