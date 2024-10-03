3cda535ad57bd5b8c1d6aecfb86145c2357e93a7
Charles <chalice.chu@gmail.com>
2024-10-02 23:22:44 -0400

    inp: google cloud secret

---
+++ frontend/components/resume/Section.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full flex items-center">
+        <div class="w-32">
+            <slot name="title" />
+        </div>
+
+        <div class="ml-4 grow">
+            <slot name="content" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ frontend/demo.ts
@@ -0,0 +1,9 @@
+import { docs } from "googleapis/build/src/apis/docs"
+
+async function main() {
+    // const res = await fetch(url)
+    // const text = await res.text
+    // console.log(text)
+
+}
+main()
