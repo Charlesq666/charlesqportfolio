e81a3c981b01941bce7f14b46691c17121523317
Charles <chalice.chu@gmail.com>
2024-10-03 01:07:18 -0400

    feat: configuer google api calling

--- frontend/demo.ts
+++ frontend/demo.ts
@@ -1,9 +1,19 @@
-import { docs } from "googleapis/build/src/apis/docs"
+import { google } from "googleapis"
 
 async function main() {
     // const res = await fetch(url)
     // const text = await res.text
     // console.log(text)
+    const docId = ""
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "./google-service-acc-key.json",
+        scopes: ["https://www.googleapis.com/auth/documents.readonly"],
+    })
+    console.log(`Authenticated as: ${auth}`)
+    google.options({auth})
+    const docs = google.docs({version: "v1"})
+    const res = await docs.documents.get({ documentId: docId })
 
+    console.log(res.data)
 }
 main()
